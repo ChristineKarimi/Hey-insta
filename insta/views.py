e4229d5b04694544614b5b061b0845b75bc01307
christinekarimi <karimikim3@gmail.com>
2018-05-10 19:12:10 +0300

    Added views

--- insta/views.py
+++ insta/views.py
@@ -7,6 +7,4 @@ def index(request):
   # Only return posts from users that are being followed
   users_followed = request.user.profile.following.all()
   posts = Post.objects.filter(profile__in=users_followed).order_by('-posted_on')
-
-
-  return render(request,'index.html',{"posts":posts})
+    return render(request,'index.html',{"posts":posts})
