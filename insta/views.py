a46a696011addd7a0a1cfb1f97da81e76449c7f4
christinekarimi <karimikim3@gmail.com>
2018-05-10 11:07:28 +0300

    Created a view function to render the index temolate

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+  # Only return posts from users that are being followed
+  users_followed = request.user.profile.following.all()
+  posts = Post.objects.filter(profile__in=users_followed).order_by('-posted_on')
+
+
+  return render(request,'index.html',{"posts":posts})
