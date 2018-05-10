4398a0e2f531602108afb7f5025de82a5c48b56c
christinekarimi <karimikim3@gmail.com>
2018-05-10 19:53:42 +0300

    Created a profile view function

--- insta/views.py
+++ insta/views.py
@@ -4,7 +4,16 @@ from django.shortcuts import render
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
-  # Only return posts from users that are being followed
+  #returns posts from users that are being followed only
   users_followed = request.user.profile.following.all()
   posts = Post.objects.filter(profile__in=users_followed).order_by('-posted_on')
     return render(request,'index.html',{"posts":posts})
+
+@login_required
+def profile(request, username):
+  user = User.objects.get(username = username)
+  if not user:
+    return redirect('Home')
+  profile = Profile.objects.get(user =user)
+  title = f"{user.username}"
+   return render(request, 'profiles/profile.html', {"title": title, "user":user, "profile":profile})    
