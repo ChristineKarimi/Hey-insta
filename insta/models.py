c1bdf13f461c93a0d968cfbfe422120cc271469e
christinekarimi <karimikim3@gmail.com>
2018-05-10 08:07:58 +0300

    Created model post

--- insta/models.py
+++ insta/models.py
@@ -7,3 +7,8 @@ class Profile(models.Model):
   phone_number = models.IntegerField(blank =True, null = True)
   location = models.CharField(max_length = 30, blank =True)
   birth_date = models.DateField(null =True, blank = True)
+
+class Post(models.Model):
+  title = models.CharField(max_length = 100)
+  image = models.ImageField(upload_to = 'posts/')
+  posted_on = models.DateTimeField(auto_now_add=True)
