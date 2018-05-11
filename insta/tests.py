c438cc5368d15469f1d9ec48ed1998f6e9ae49d8
christinekarimi <karimikim3@gmail.com>
2018-05-11 15:18:43 +0300

    Testcase for the save image functionality

--- insta/tests.py
+++ insta/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Post,Profile
 
 # Create your tests here.
+
+class ImageTestClass(TestCase):
+
+    def setUp(self):
+
+        self.user =User.objects.create_user(username:'misskim',password:'ckk')
+
+        self.image = Image.objects.create(image_link='posts/image1.png',name ='flower',caption='beauty of life',created_by=self.user)
+
+    def test_instance(self):
+
+        self.image.save()
+
+        self.assertTrue(isinstance(self.image,Image))
