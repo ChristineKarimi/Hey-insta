b31a9fbd22664cf80ed44669594bff3c85b8dd1d
christinekarimi <karimikim3@gmail.com>
2018-05-12 14:14:10 +0300

    View function to enable a user to add likes

--- insta/views.py
+++ insta/views.py
@@ -103,4 +103,24 @@ def likes(request, pk):
 
   title = 'Likes'
   return render(request, 'follow_list.html', {"title": title, "profiles":profiles})  
+
+@ajax_request
+@login_required
+def add_like(request):
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    try:
+        like = Like(post=post, user=request.user)
+        like.save()
+        result = 1
+    except Exception as e:
+        like = Like.objects.get(post=post, user=request.user)
+        like.delete()
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk
+    }
+  
   
