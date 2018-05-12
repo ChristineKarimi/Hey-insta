8a9a15cec3bae58c3579276a8590a2702614aaeb
christinekarimi <karimikim3@gmail.com>
2018-05-12 16:46:59 +0300

    Added the toggle heart functionality and implemented ajax

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,4 @@
-// for the likes
+// for likes
 
 function create_like(success_cb, error_cb) {
   var post_pk = $(this).siblings('.hidden-data').find('.post-pk').text();
@@ -16,3 +16,20 @@ function create_like(success_cb, error_cb) {
 }
 
 
+function error_cb(error) {
+  console.log(error);
+}
+
+
+function like_update_view(data) {
+  console.log(data);
+
+  // the toggle heart icon
+  var $hiddenData = $('.hidden-data.' + data.post_pk);
+  if (data.result) {
+    $hiddenData.siblings('.submit-like').removeClass('fa-heart-o').addClass('fa-heart');
+  } else {
+    $hiddenData.siblings('.submit-like').removeClass('fa-heart').addClass('fa-heart-o');
+  }
+
+ 
