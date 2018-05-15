956391fbbc68b4cac0546916bc830f9bc49cf47a
christinekarimi <karimikim3@gmail.com>
2018-05-15 09:49:44 +0300

    Added tests for  the comments model

--- insta/tests.py
+++ insta/tests.py
@@ -8,9 +8,12 @@ class PostTestClass(TestCase):
 
     def setUp(self):
 
-        self.user =User.objects.create_user('misskim','ckk')
+        self.user = User.objects.create_user('kim','chris')
 
-        self.post = Post.objects.create(image_link='posts/1.png',name ='flower',caption='beauty of life',created_by=self.user)
+        self.post = Post.objects.create(image_link='posts/1.png',
+                                              name ='flower',
+                                              caption='beauty of life',
+                                              created_by=self.user)
 
     def test_instance(self):
 
@@ -20,15 +23,15 @@ class PostTestClass(TestCase):
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------        
 '''
-saving post functionality test
+saving a post functionality test
 
 '''
 
     def test_save_method(self):
 
-        self.new_image.save_image()
+        self.new_Post.save_image()
 
-        images = Image.objects.all()
+        images = Post.objects.all()
 
         self.assertTrue(len(images)>0)        
 
@@ -100,8 +103,90 @@ class ProfileTestClass(TestCase):
 
     def setUp(self):
 
-        self.profile=Profile(profile_photo='pics/image1.jpg',Bio ='full stack dev')
+        self.profile=Profile(profile_photo='pics/image1.jpg',
+                                      Bio ='full stack dev')
 
         self.profile.save()
 
-#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------        
+    def test_instance(self):
+
+        self.assertTrue(isinstance(self.profile,Profile))
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+'''
+save method test
+'''
+def test_save_method(self):
+
+        self.profile.save_profile()
+
+        profile = Profile.objects.all()
+
+        self.assertTrue(len(profile)>0)
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
+
+def test_delete_method(self):
+
+        self.profile.save_profile()
+
+        profile = Profile.objects.all()
+
+        self.profile.delete_profile()
+
+        self.assertTrue(len(profile)==0)
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------        
+'''
+update profile test
+
+'''
+def test_update_method(self):
+
+        new_profile = 'pics/7.png','code more'
+
+        self.profile.update_profile(self.profile.id,new_profile)
+
+        new_profile = Profile.objects.filter(profile_photo='pics/7.png',
+                                                       Bio='code more and more')
+
+        self.assertTrue(len(new_profile)==1)       
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+class CommentTestClass(TestCase):
+   """
+   test case fo the comments
+   
+   """
+
+# class CommentTestClass(TestCase):
+
+#     def setUp(self):
+
+#         self.comment=Comment(profile_photo='pics/image1.jpg',Bio ='full stack dev')
+
+#         self.comment.save()
+
+#     def test_instance(self):
+
+#         self.assertTrue(isinstance(self.comment,Comment))        
+
+
+
+   def setUp(self):
+       self.new_user = User.objects.create_user("testuser", "chris")
+
+       self.new_profile_t = Profile(profile_pic='profiles/profile1.jpg',
+                                    profile_bio="full stack dev based in nairobi",
+                                    profile_owner=self.new_user)
+       self.new_profile_t.save()
+
+       self.new_image = Image(image='photos/1.jpeg', image_name='kim',
+                              image_caption="test image1", image_created_by=self.new_profile_t)
+       self.new_image.save()
+
+       self.new_comm = Comment(
+           comment_image=self.new_image, comment_owner=self.new_profile_t, comment_post="test comment")
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+           
