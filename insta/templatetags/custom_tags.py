8a78392e72b4c813787001ba0b4848192b4770ec
christinekarimi <karimikim3@gmail.com>
2018-05-16 09:05:44 +0300

    Added custom styles

--- insta/templatetags/custom_tags.py
+++ insta/templatetags/custom_tags.py
@@ -18,27 +18,6 @@ def has_user_liked_post(post, user):
 def is_following(profile, profile_to_check):
     return profile.following.filter(user_id=profile_to_check.user_id).exists()
 
-#addcss filter
-# custom filters to add css in django forms
-@register.filter(name='addClass')
-def addClass(field, css):
-   return field.as_widget(attrs={"class":css})
-
-#Adds the given class to the class attribute of the given field.
-
-@register.filter(name='addID')
-def addID(field, css):
-   return field.as_widget(attrs={"id":css})
-
-@register.filter(name='parse_hashtags')
-def parse_hashtags(field):
-    hashtags_arr = re.findall(r"#(\w+)", field)
-    for hashtag in hashtags_arr:
-        html_tag = "<a href='/explore?hashtag=" + hashtag + "'>#" + hashtag + "</a>"
-        field = field.replace("#" + hashtag, html_tag)
-    return field
-
-
 @register.simple_tag(takes_context=True)
 def active(context, pattern_or_urlname):
     try:
