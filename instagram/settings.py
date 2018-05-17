53b0ec076b1eead1e57efc21e041c1bdd1f3cfb7
christinekarimi <karimikim3@gmail.com>
2018-05-17 16:28:14 +0300

    heroku redeployment

--- instagram/settings.py
+++ instagram/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'PASSWORD':'karimi',
     }
 }
-DATABASES['default'].update(db_from_env)c
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
